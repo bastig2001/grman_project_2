551ed05f4272c959f3b99d6f126ce8fedeb19348
Sebastian Grman <sebastian.grman@gmail.com>
2021-02-15 10:07:13 +0100

    add spdlog for logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include "config.h"
 
-#include <iostream>
+#include <spdlog/spdlog.h>
 
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
     auto config_result = configure(argc, argv);
+    spdlog::set_level(spdlog::level::debug);
     
     if (auto config = get_if<Config>(&config_result)) {
-        cout << "This program was called correctly.\n"
-             << "The config is:\n" << (string)*config << endl;
+        spdlog::debug(
+            "This program was called correctly.\n"
+            "The config is:\n{}", (string)*config
+        );
     }
     else {
         return get<int>(config_result);
