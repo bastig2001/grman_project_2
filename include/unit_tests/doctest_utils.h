66d4000d189a2b82d32d0381da959994b1a776ad
Sebastian Grman <sebastian.grman@gmail.com>
2021-03-06 16:01:58 +0100

    put backslash back in line

--- include/unit_tests/doctest_utils.h
+++ include/unit_tests/doctest_utils.h
@@ -9,6 +9,6 @@
     static size_t _doctest_subcase_idx = 0;                                                     \
     std::for_each(data_container.begin(), data_container.end(), [&](const auto& in) {           \
         DOCTEST_SUBCASE((std::string(#data_container "[") +                                     \
-                        std::to_string(_doctest_subcase_idx++) + "]").c_str()) { data = in; }  \
+                        std::to_string(_doctest_subcase_idx++) + "]").c_str()) { data = in; }   \
     });                                                                                         \
     _doctest_subcase_idx = 0
